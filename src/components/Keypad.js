5b7d822c37f098d66d7c43f311ffc1deb481356f
Myles Reyburn <mylesreyburn@gmail.com>
2023-02-24 22:19:26 -0700

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
+function keypadFunctionality(event){
+    console.log("Entering password...");
+
+}
+
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={keypadFunctionality}></input>
+        </div>
     )
 }
 
